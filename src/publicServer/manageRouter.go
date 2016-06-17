a02733a7793c94c79a5f47df47a258c75378f18f
Levy <zly.george@163.com>
2016-06-18 00:53:38 +0800

    successful auth

--- src/publicServer/manageRouter.go
+++ src/publicServer/manageRouter.go
@@ -5,12 +5,31 @@ import (
     "github.com/levythu/gurgling/midwares/cookie"
     "math/rand"
     "time"
+    "crypto/tls"
+    "net/http"
+    "io/ioutil"
     "net/url"
+    "fmt"
+    "strings"
 )
+
+var pipeClient=&http.Client{}
+var piptClientSSL=&http.Client{Transport: &http.Transport{
+	TLSClientConfig: &tls.Config{},
+}}
+var client *http.Client
 
 func init() {
     rand.Seed(time.Now().UnixNano())
+    client=pipeClient
+    if r, e:=url.Parse(VERIFY_URL); e==nil {
+        if strings.ToLower(r.Scheme)=="https" {
+            client=piptClientSSL
+        }
+    }
 }
+
+
 const strFarm="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
 func generateRandStr(length int) string {
     var ret=make([]byte, length)
@@ -21,17 +40,50 @@ func generateRandStr(length int) string {
     return string(ret)
 }
 
+func VerifyRequest(token string) bool {
+    var proxyRequest, err=http.NewRequest("GET", VERIFY_URL+"?token="+token, strings.NewReader(""))
+    if err!=nil {
+        return false
+    }
+
+    var proxyResponse, err2=client.Do(proxyRequest)
+    if err2!=nil {
+        return false
+    }
+
+    var content, err3=ioutil.ReadAll(proxyResponse.Body)
+    defer proxyResponse.Body.Close()
+    if err3!=nil {
+        return false
+    }
+
+    fmt.Println(string(content))
+    if string(content)=="TRUE" {
+        return true
+    }
+    return false
+}
+
 func CreateManageRouter() Router {
     var r=ARouter().Use(cookie.ASession(generateRandStr(50)))
     r.Get("/callback", func(req Request, res Response) {
-        res.Send("123")
+        if VerifyRequest(req.Query()["token"]) {
+            var session=req.F()["session"].(map[string]string)
+            session["user"]="authedUser"
+            res.Redirect("/admin")
+        } else {
+            res.Redirect("/admin/fail")
+        }
+    })
+    r.Get("/fail", func(req Request, res Response) {
+        res.Send("Auth fail.")
     })
     r.Use(func(req Request, res Response) bool {
         var session=req.F()["session"].(map[string]string)
         if _, ok:=session["user"]; !ok {
             var parameters=url.Values{}
 	        parameters.Add("destination", HOSTNAME_AND_SCHEMA+"/admin/callback")
-            res.Redirect(VERIFY_URL+"?"+parameters.Encode())
+            res.Redirect(CREATE_URL+"?"+parameters.Encode())
             return false
         } else {
             res.Send("Hello!")
--- src/publicServer/setting.go
+++ src/publicServer/setting.go
@@ -1,4 +1,5 @@
 package main
 
-const VERIFY_URL="https://www.levy.at/a/auth/ask"
+const CREATE_URL="https://www.levy.at/a/auth/ask"
 const HOSTNAME_AND_SCHEMA="http://thu.levy.at:2335"
+const VERIFY_URL="https://www.levy.at/rest/nonauthorized/auth/test"
