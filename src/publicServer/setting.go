cfdc3c34a3c0652550a2591d92933f88fc66ef16
Levy <zly.george@163.com>
2017-02-14 23:17:14 -0500

    update auth server addr

--- src/publicServer/setting.go
+++ src/publicServer/setting.go
@@ -1,5 +1,5 @@
 package main
 
-const CREATE_URL="https://www.levy.at/a/auth/ask"
+const CREATE_URL="https://levy.at/a/auth/ask"
 const HOSTNAME_AND_SCHEMA="http://thu.levy.at:2335"
-const VERIFY_URL="https://www.levy.at/rest/nonauthorized/auth/test"
+const VERIFY_URL="https://levy.at/rest/nonauthorized/auth/test"
